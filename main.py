f237bc719989a948b428be40f8ebd8ac2de3c99a
Alvaro <alvaro.barrena02@gmail.com>
2024-11-12 14:46:41 +0100

    improving the code

--- main.py
+++ main.py
@@ -8,53 +8,64 @@ menu = """
 4. Exit
 """
 
+print(menu)
 while True:
     option = input("Enter an option (1-4): ")
-
+    print(menu)
     match option:
         case "1":
             print("##### Read an article #####")
 
             # URL of the article to convert to audio
             url = input("Enter the URL of the article: ")
-            print("\nReading the article, please wait...")
+            
+            if url == "exit":
+                break
+            else:
+                print("\nReading the article, please wait...")
 
-            # Create an instance of the ReadContent class
-            reader = content.ReadContent(url)
-            reader.read_content()
+                # Create an instance of the ReadContent class
+                reader = content.ReadContent(url)
+                reader.read_content()
 
             # Add a delay to avoid the program from closing immediately
             input("Press Enter to continue...")
-            print(menu)
 
         case "2":
             print("##### Read a text file #####")
             # Path of the text file to read
             file_path = input("Enter the path of the text file (example.txt): ")
             
-            # Create an instance of the ReadContent class
-            reader = content.ReadContent(file_path, is_url=False)
-            reader.read_content()
+            if file_path == "exit":
+                break
+            else:
+                # Create an instance of the ReadContent class
+                reader = content.ReadContent(file_path, is_url=False)
+                reader.read_content()
+                print("Reading the text file, please wait...")
 
-            # Add a delay to avoid the program from closing immediately
-            input("\nPress Enter to continue...")
-            print(menu)
+                # Add a delay to avoid the program from closing immediately
+                input("\nPress Enter to continue...")
+                print(menu)
 
         case "3":
             print("##### Enter text manually #####")
             text = input("Enter the text you want to convert to speech: ")
+            
+            if text == "exit":
+                break
+            else:
+                # Create an instance of the ReadContent class
+                reader = content.ReadContent()
+                reader.add_manual_text(text)
 
-            # Create an instance of the ReadContent class
-            reader = content.ReadContent()
-            reader.add_manual_text(text)
+                # Read the manually entered text
+                print("Reading the text written please wait...")
+                reader.read_content()
 
-            # Read the manually entered text
-            print("Reading text...")
-            reader.read_content()
-
-            # Add a delay to avoid the program from closing immediately
-            input("\nPress Enter to continue...")
-            print(menu)
+                # Add a delay to avoid the program from closing immediately
+                input("\nPress Enter to continue...")
+                print(menu)
 
         case "4":
             print("Exiting the program...")
